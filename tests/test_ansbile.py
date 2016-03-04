42e976092353ded491e64d5e0e4bc8c8c91d799e
nshinde5486 <nilesh.shinde@hpe.com>
2016-03-03 16:27:12 -0800

    new: dev: topology framework with connectivity between control machine and switch
    Change-Id: Ief5c8e23d0972859b08ebc53997a4a45fafcdaf7
    Signed-off-by: Nilesh Shinde<nilesh.shinde@hpe.com>

---
+++ tests/test_ansbile.py
@@ -0,0 +1,48 @@
+TOPOLOGY = """
+#         +-------------+
+#         |             |
+#         |             |
+#         | OpenSwitch 1|
+#         |             |
+#         |             |
+#         |             |
+#         +------+------+
+#                |1
+#                |
+#              1 |
+#         +------+------+
+#         |             |
+#         |             |
+#         |  Host 1     |
+#         | (control    |
+#         |  machine)   |
+#         +-------------+
+#
+# Nodes
+[type=openswitch name="OpenSwitch 1"] ops1
+[type=host name="Host 1" image="openswitch/ansible:latest"] hs1
+[force_name=eth0]ops1:1
+#
+# Links
+hs1:1 -- ops1:1
+"""
+
+
+def test_topology(topology, step):
+    ops1 = topology.get('ops1')
+    hs1 = topology.get('hs1')
+
+    assert ops1 is not None
+    assert hs1 is not None
+
+    # configure IP on host
+    hs1.libs.ip.interface('1', addr='10.10.10.1/24', up=True)
+
+    # configure IP on switch for interface 1
+    with ops1.libs.vtysh.ConfigInterface('1') as ctx:
+        ctx.ip_address('10.10.10.2/24')
+        ctx.no_shutdown()
+
+    # Check the connectivity with host
+    ping = hs1.libs.ping.ping(2, '10.10.10.2', 0.1)
+    assert ping['transmitted'] == ping['received'] == 2
